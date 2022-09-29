1ee12538119207bb8488242ca30873632ce63985
Miguel Gomes <miguelrgomes1698@gmail.com>
2022-09-29 11:38:16 +0100

    about page done

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <>
+      <div class="h-screen flex items-center mx-auto">
+        <h1>Text talking about the company, etc</h1>
+      </div>
+    </>
+  )
+}
--- pages/profiles/post.js
+++ pages/profiles/post.js
@@ -6,7 +6,7 @@ import {ChatBubbleLeftIcon, HeartIcon, ShareIcon, BookmarkIcon} from '@heroicons
 export default function Post() {
     return (
         <>
-            <div class="h-full w-full flex items-center justify-center my-10">
+            <div class="h-full w-full flex items-center justify-center mt-2 lg:mt-6 mb-4">
                 <div class="w-10/12">
                     <div class="flex items-center justify-between">
                         <div class="gap-3.5	flex items-center ">
@@ -19,42 +19,42 @@ export default function Post() {
                             />
                             <div class="flex flex-col">
                                 <b class="mb-2 capitalize">sofia müller</b>
-                                <time datetime="06-08-21" class="text-gray-400 text-xs">06 August at 09.15 PM
+                                <time datetime="06-08-21" class="text-xs">06 August at 09.15 PM
                                 </time>
                             </div>
                         </div>
                     </div>
-                    <div class="whitespace-pre-wrap my-7">Hello guys ?</div>
+                    <div class="my-2">Hello guys ?</div>
                         <Image
                             className="rounded-2xl"
                             layout="responsive"
-                            width="55vw"
-                            height="55vw"
+                            width="1vw"
+                            height="1vw"
                             src="/assets/feet.jpeg"
                             alt="Your Profile" 
                         />
-                        <div class="container grid grid-cols-2 sm:grid-cols-4 h-16 my-8">
-                            <div class="mx-auto">
-                                <ChatBubbleLeftIcon className="h-6 w-6" aria-hidden="true" />
-                                <div class="text-sm	">10 Comments</div>
+                        <div class="container grid grid-cols-4 sm:grid-cols-4 my-6">
+                            <div>
+                                <ChatBubbleLeftIcon className="h-6 w-6 mx-auto" aria-hidden="true" />
+                                <div class="text-xs text-center">10 Comments</div>
                             </div>
-                            <div class="mx-auto">
-                                <HeartIcon className="h-6 w-6" aria-hidden="true" />
-                                <div class="text-sm">5 Likes</div>
+                            <div>
+                                <HeartIcon className="h-6 w-6 mx-auto" aria-hidden="true" />
+                                <div class="text-xs text-center">5 Likes</div>
                             </div>
-                            <div class="mx-auto">
-                                <ShareIcon className="h-6 w-6" aria-hidden="true" />
-                                <div class="text-sm">Share</div>
+                            <div>
+                                <ShareIcon className="h-6 w-6 mx-auto" aria-hidden="true" />
+                                <div class="text-xs text-center">Share</div>
                             </div>
-                            <div class="mx-auto">
-                                <BookmarkIcon className="h-6 w-6" aria-hidden="true" />
-                                <div class="text-sm">Saved</div>
+                            <div>
+                                <BookmarkIcon className="h-6 w-6 mx-auto" aria-hidden="true" />
+                                <div class="text-xs text-center">Save</div>
                             </div>
                         </div>
-                        <div class="flex items-center justify-between mt-4">
+                        <div class="flex mt-4">
                             <Image class="rounded-full" src="/assets/profile_photo.jpeg" width="55vw" height="55vw"/>
-                            <div class="h-11 w-10/12 border rounded-2xl overflow-hidden">
-                                <input type="text" class="h-full w-full outline-none rounded-2xl" placeholder="  Write your comment..." name="comment"/>
+                            <div class="h-11 w-10/12 border rounded-2xl overflow-hidden ml-4">
+                                <input type="text" class="h-full w-full rounded-2xl" placeholder="  Write your comment..." name="comment"/>
                             </div>
                         </div>
                 </div>
